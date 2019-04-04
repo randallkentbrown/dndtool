b0a035fc676b1e034f6d887e04214041ae3aaecf
Kent Brown <randall.k.brown.jr@gmail.com>
2019-04-04 13:47:58 -0400

    Initial repository commit.

---
+++ src/dndtool/Main.java
@@ -0,0 +1,21 @@
+/* created by Kent Brown on 4/4/2019 */
+
+/* package */
+package dndtool;
+
+/* imports */
+
+/* implementation */
+public class Main {
+
+    // STATE //
+
+    // CONSTRUCTOR //
+
+    // GETTERS & SETTERS //
+
+    // BEHAVIOUR //
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
